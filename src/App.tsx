3bfc60dc0cf27100c9118515cdb16b645a238c83
Liam Moroney <liam.moroney14@gmail.com>
2019-12-18 12:16:54 +0000

    Updates dependencies and added check for null localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,20 @@ import { Provider } from "react-redux";
 import configureStore from "./configureStore";
 import Root from "./Containers/Root";
 
+export const loadState = (stateName: string) => {
+  try {
+    const serializedState = localStorage.getItem(stateName);
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
 const store = configureStore({
-  authenticate: JSON.parse(localStorage.getItem("authenticate") || "")
+  authenticate: loadState("authenticate")
 });
 
 // persist authenticate data
